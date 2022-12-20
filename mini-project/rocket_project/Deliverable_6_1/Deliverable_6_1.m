dcc0fb4fac5443456774378a421a5ac0ad4eb0da
jiaruiyu99 <114936217+jiaruiyu99@users.noreply.github.com>
2022-12-20 21:06:33 +0100

    done 6.1

--- mini-project/rocket_project/Deliverable_6_1/Deliverable_6_1.m
+++ mini-project/rocket_project/Deliverable_6_1/Deliverable_6_1.m
@@ -1,3 +1,41 @@
+clc;
+clear;
+close all;
 addpath(fullfile('..', 'src'));
 
 %% TODO: This file should produce all the plots for the deliverable
+Ts = 1/20;
+rocket = Rocket(Ts);
+H = 5; % Horizon length in seconds
+nmpc = NmpcControl(rocket, H);
+% MPC reference with default maximum roll = 15 deg
+ref = @(t_, x_) ref_EPFL(t_);
+% MPC reference with specified maximum roll = 50 deg
+roll_max = deg2rad(50);
+ref = @(t_, x_) ref_EPFL(t_, roll_max);
+% Evaluate once and plot optimal open−loop trajectory,
+% pad last input to get consistent size with time and state
+x = zeros(12, 1);
+x0 =x;
+% [u, T_opt, X_opt, U_opt] = nmpc.get_u(x, ref);
+% U_opt(:,end+1) = nan;
+% ph = rocket.plotvis(T_opt, X_opt, U_opt, ref);
+Tf = 30;
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
+rocket.anim_rate = 10; % Increase this to make the animation faster
+ph = rocket.plotvis(T, X, U, Ref);
+
+%%
+Ts = 1/20;
+rocket = Rocket(Ts);
+[xs, us] = rocket.trim(); 
+sys = rocket.linearize(xs, us);
+[sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us);
+mpc_x = MpcControl_x(sys_x, Ts, H);
+mpc_y = MpcControl_y(sys_y, Ts, H);
+mpc_z = MpcControl_z(sys_z, Ts, H);
+mpc_roll = MpcControl_roll(sys_roll, Ts, H);
+mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @mpc.get_u, ref);
+rocket.anim_rate = 10;
+ph = rocket.plotvis(T, X, U, Ref);
---
+++ mini-project/rocket_project/Deliverable_6_1/NmpcControl.m
@@ -0,0 +1,214 @@
+classdef NmpcControl < handle
+    
+    properties
+        solver
+        nx, nu, N
+        nlp_x0
+        nlp_lbx, nlp_ubx
+        nlp_lbg, nlp_ubg
+        nlp_p
+        
+        T_opt
+        sol
+        idx
+        
+        % Warmstart
+        nlp_lam_x0
+        nlp_lam_g0
+    end
+    
+    methods
+        function obj = NmpcControl(rocket, tf)
+           
+            import casadi.*
+            
+            N_segs = ceil(tf/rocket.Ts); % MPC horizon
+            nx = 12; % Number of states
+            nu = 4;  % Number of inputs
+            
+            % Decision variables (symbolic)
+            N = N_segs + 1; % Index of last point
+            X_sym = SX.sym('X_sym', nx, N); % state trajectory
+            U_sym = SX.sym('U_sym', nu, N-1); % control trajectory)
+            
+            % Parameters (symbolic)
+            x0_sym  = SX.sym('x0_sym', nx, 1);  % initial state
+            ref_sym = SX.sym('ref_sym', 4, 1);  % target position
+            
+            % Default state and input constraints
+            ubx = inf(nx, 1);
+            lbx = -inf(nx, 1);
+            ubu = inf(nu, 1);
+            lbu = -inf(nu, 1);
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            % set lbx, ubx, lbu, ubu defined above
+            ubx(5,1 ) = deg2rad(80);
+            lbx(5,1 ) = -deg2rad(80);
+            ubu(1,1 ) = deg2rad(15);
+            lbu(1,1 ) = -deg2rad(15);
+            ubu(2,1 ) = deg2rad(15);
+            lbu(2,1 ) = -deg2rad(15);
+            ubu(3,1 ) = 80;
+            lbu(3,1 ) = 50;
+            ubu(4,1 ) = 20;
+            lbu(4,1 ) = -20;
+
+            % get terminal cost
+            [xs, us] = rocket.trim(); % ref pose is vertical to ground
+            % Terminal Cost and Terminal Set
+            lin_sys = rocket.linearize(xs, us);
+            lin_sys_d = c2d(lin_sys, rocket.Ts);    
+            sys = LTISystem('A', lin_sys_d.A, 'B', lin_sys_d.B);
+            Q = diag([25;25;25;10;10;10;10;10;35;70;70;100]);
+            R = eye(nu)*0.001;
+            sys.x.penalty = QuadFunction(Q); 
+            sys.u.penalty = QuadFunction(R);
+            sys.u.min = lbu; 
+            sys.u.max = ubu;
+            sys.x.min = lbx; 
+            sys.x.max = ubx;
+            Qf = sys.LQRPenalty.weight;
+
+            % discrete dynamic
+            f = @(x, u) rocket.f(x, u);
+            f_dis = @(x, u) RK4(x, u, rocket.Ts, f);
+
+            % Xs, Us
+%             Xs  = SX.sym('Xs', nx, 1);  % steady state
+%             Us  = SX.sym('Us', nu, 1);  % steady state
+            Xs = [xs(1:5); ref_sym(4); xs(7:9); ref_sym(1:3)];
+            Us = us;
+            
+            %cost
+            cost = 0;
+            eq_constr = [X_sym(:,1) - x0_sym];
+%             eq_constr = [];
+            ineq_constr = [];
+            for i = 1:N-1
+                cost = cost + (X_sym(:,i)-Xs)' * Q * (X_sym(:,i)-Xs)...
+                + (U_sym(:, i)-Us)' * R * (U_sym(:, i)-Us);
+                eq_constr = [eq_constr ; (X_sym(:, i+1) - f_dis(X_sym(:, i), U_sym(:,i)))];
+                ineq_constr = [ineq_constr; X_sym(5,i) - ubx(5, 1) ; -(X_sym(5,i) - lbx(5, 1)); U_sym(:,i) - ubu ; -(U_sym(:,i) - lbu)];
+            end
+            cost =  cost + X_sym(:,N)'*Qf*X_sym(:,N);
+            
+            % Equality constraints (Casadi SX), each entry == 0
+
+            
+            % Inequality constraints (Casadi SX), each entry <= 0
+
+
+            % For box constraints on state and input, overwrite entries of
+            % lbx, ubx, lbu, ubu defined above
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % ---- Assemble NLP ------
+            nlp_x = [X_sym(:); U_sym(:)];
+            nlp_p = [x0_sym; ref_sym];
+            nlp_f = cost;
+            nlp_g = [eq_constr; ineq_constr];
+            
+            nlp = struct('x', nlp_x, 'p', nlp_p, 'f', nlp_f, 'g', nlp_g);
+            
+            % ---- Setup solver ------
+            opts = struct('ipopt', struct('print_level', 0), 'print_time', false);
+            obj.solver = nlpsol('solver', 'ipopt', nlp, opts);
+            
+            % ---- Assemble NLP bounds ----
+            obj.nlp_x0  = zeros(size(nlp_x));
+            
+            obj.nlp_ubx = [repmat(ubx, N, 1); repmat(ubu, (N-1), 1)];
+            obj.nlp_lbx = [repmat(lbx, N, 1); repmat(lbu, (N-1), 1)];
+            
+            obj.nlp_ubg = [zeros(size(eq_constr)); zeros(size(ineq_constr))];
+            obj.nlp_lbg = [zeros(size(eq_constr)); -inf(size(ineq_constr))];
+            
+            obj.nlp_p = [zeros(size(x0_sym)); zeros(size(ref_sym))];
+            
+            obj.nlp_lam_x0 = [];
+            obj.nlp_lam_g0 = [];
+            
+            obj.nx = nx;
+            obj.nu = nu;
+            obj.N = N;
+            obj.T_opt = linspace(0, N * rocket.Ts, N);
+            
+            obj.idx.X = [1, obj.N * obj.nx];
+            obj.idx.U = obj.idx.X(2) + [1, (obj.N-1) * obj.nu];
+            obj.idx.u0 = obj.idx.U(1) + [0, obj.nu-1];
+        end
+        
+        function [u, T_opt, X_opt, U_opt] = get_u(obj, x0, ref)
+            
+            obj.solve(x0, ref);
+            
+            % Evaluate u0
+            nlp_x = obj.sol.x;
+            id = obj.idx.u0;
+            u = full( nlp_x(id(1):id(2)) );      
+            
+            if nargout > 1, T_opt = obj.get_T_opt(); end
+            if nargout > 2, X_opt = obj.get_X_opt(); end
+            if nargout > 3, U_opt = obj.get_U_opt(); end
+            return
+            
+            % Additional evaluation
+            % Complete trajectory
+            % % X_opt = full(reshape(nlp_x(idx_X(1):idx_X(2)), obj.nx, obj.N));
+            % % U_opt = full(reshape(nlp_x(idx_U(1):idx_U(2)), obj.nu, obj.N - 1));
+            % %
+            % % cost_opt = full(sol.f);
+            % % constr_opt = full(sol.g);
+            % %
+            % % stats = obj.solver.stats;
+        end
+        
+        function solve(obj, x0, ref)
+            
+            % ---- Set the initial state and reference ----
+            obj.nlp_p = [x0; ref];     % Initial condition
+            obj.nlp_x0(1:obj.nx) = x0; % Initial guess consistent
+            
+            % ---- Solve the optimization problem ----
+            args = {'x0', obj.nlp_x0, ...
+                'lbg', obj.nlp_lbg, ...
+                'ubg', obj.nlp_ubg, ...
+                'lbx', obj.nlp_lbx, ...
+                'ubx', obj.nlp_ubx, ...
+                'p', obj.nlp_p, ...
+                %                 'lam_x0', obj.nlp_lam_x0, ...
+                %                 'lam_g0', obj.nlp_lam_g0
+                };
+            
+            obj.sol = obj.solver(args{:});
+            if obj.solver.stats.success ~= true
+                solve_status_str = obj.solver.stats.return_status;
+                fprintf([' [' class(obj) ': ' solve_status_str '] ']);
+                obj.sol.x(obj.idx.u0) = nan;
+            end
+            
+            % Use the current solution to speed up the next optimization
+            obj.nlp_x0 = obj.sol.x;
+            obj.nlp_lam_x0 = obj.sol.lam_x;
+            obj.nlp_lam_g0 = obj.sol.lam_g;
+        end
+        function T_opt = get_T_opt(obj)
+            T_opt = obj.T_opt;
+        end
+        function X_opt = get_X_opt(obj)
+            nlp_x = obj.sol.x;
+            id = obj.idx.X;
+            X_opt = full(reshape(nlp_x(id(1):id(2)), obj.nx, obj.N));
+        end
+        function U_opt = get_U_opt(obj)
+            nlp_x = obj.sol.x;
+            id = obj.idx.U;
+            U_opt = full(reshape(nlp_x(id(1):id(2)), obj.nu, obj.N - 1));
+        end
+    end
+end
+
